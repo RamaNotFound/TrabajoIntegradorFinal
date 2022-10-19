6bb5db5bd56114a7487a09156a36a4185e791ce7
RamaNotFound <ramirovillarreal636@gmail.com>
2022-10-19 17:11:12 -0300

    cambios
    retire el asignar id de veiculosmes y actualize ambos objetos para heredar la funcion de veiculos

--- VeiculosHora.py
+++ VeiculosHora.py
@@ -1,4 +1,7 @@
+from Veiculos import Veiculo
 class VeiculosHora:
     def __init__(self, horas, Categoria):
         self.horas = horas
         self.Categoria = Categoria
+    def mostrar_datos(self):
+        return super().mostrar_datos() + f"Cantidad de horas: {self.horas}\n"
--- VeiculosMes.py
+++ VeiculosMes.py
@@ -1,6 +1,7 @@
+from Veiculos import Veiculo
 class VeiculosMes:
     def __init__(self, Id, Categoria):
         self.Id = Id
         self.Categoria = Categoria
-    def Asignar_Id(self):
-    
+    def mostrar_datos(self):
+        return super().mostrar_datos() + f"Id: {self.Id}\n"
