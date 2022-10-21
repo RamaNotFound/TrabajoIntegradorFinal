b1d5019792bb9f265ce3e1c59f01c99d96b893da
RamaNotFound <ramirovillarreal636@gmail.com>
2022-10-21 12:57:54 -0300

    Decidi sacar los tipos de vehiculos

--- VehiculosHora.py
+++
@@ -1,7 +0,0 @@
-from vehiculos import vehiculo
-class vehiculosHora:
-    def __init__(self, horas, Slot):
-        self.horas = horas
-        self.Categoria = Categoria
-    def mostrar_datos(self):
-        return super().mostrar_datos() + f"Cantidad de horas: {self.horas}\n"
--- VehiculosMes.py
+++
@@ -1,7 +0,0 @@
-from vehiculos import vehiculo
-class vehiculosMes:
-    def __init__(self, Id, Slot):
-        self.Id = Id
-        self.Categoria = Categoria
-    def mostrar_datos(self):
-        return super().mostrar_datos() + f"Id: {self.Id}\n"
